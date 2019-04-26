78e4f7bfafc9997bd5d82dacfdc588adc7e14080
Anne Engin <anne@Annes-MacBook-Air.local>
2019-04-25 22:36:21 -0400

    header rendered

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import ImageCard from "./components/ImageCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import Cards from "./cards.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+
+
+  render() {
+    return (
+      <Wrapper>
+        <Title>Greys Anatomy</Title>
+        {Cards.map(card => (
+          <imageCard
+            id={card.id}
+            key={card.id}
+            image={card.image}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/ImageCard/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./style.css";
+
+
+function ImageCard(props) {
+  return (
+
+    <div className="card">
+      <div className="img-container">
+        <img alt={props.id} src={props.image} />
+      </div>
+    </div>
+
+    // <div onClick = {props.clickImage} className="col-md-3">
+    // <img src={props.imageUrl}  className="img-responsive" alt="greysanatomy"/>
+    // </div>
+  )
+}
+
+export default ImageCard;
