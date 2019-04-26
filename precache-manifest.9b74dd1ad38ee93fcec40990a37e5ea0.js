1cb16f448aca237a16d24e7fa7f1814f7220950c
Anne Engin <anne@Annes-MacBook-Air.local>
2019-04-25 23:49:26 -0400

    Updates

---
+++ precache-manifest.9b74dd1ad38ee93fcec40990a37e5ea0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a13875946ebe620d57e0e0166f32f042",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "8b1631a6290870a36d1f",
+    "url": "/clicky-game/static/css/main.b31860c2.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/clicky-game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "8b1631a6290870a36d1f",
+    "url": "/clicky-game/static/js/main.cd8ca715.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
